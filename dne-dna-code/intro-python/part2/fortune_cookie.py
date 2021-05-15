7fc98379f95fbe1c91902c1a3ec8cd42657646a1
Anatoly <tas@uszr.ru>
2021-05-14 17:02:18 -0700

    'Jason'

--- dne-dna-code/intro-python/part2/fortune_cookie.py
+++ dne-dna-code/intro-python/part2/fortune_cookie.py
@@ -34,6 +34,12 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
     # cookie's message.
+    fortuna=generate_fortune()
+    lucky=generate_lucky_numbers(how_many_lucky_numbers)
+    for x in lucky:
+        x=str(x)
+        fortuna=fortuna+x
+    return fortuna
 
     raise NotImplementedError()
 
